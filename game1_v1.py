a359753004d3819093093c0717e42d715477520c
Konstantin Kulikin <konstantin2000@gmail.com>
2019-12-30 13:51:25 +0200

    удалил файл

--- game1_v1.py
+++ game1_v1.py
@@ -4,67 +4,77 @@ from random import randint
 # Константы
 WIDTH = 800
 HEIGHT = 600
-DELAY = 30
+DELAY = 50
+TIMER = 3000
 
 
-def left_click(event):
-    # создание пулек
-    r = 20
+def pulja(event):
+    '''создание пулек'''
+    r = 10
     x = event.x
     y = event.y
     vx = 20
     vy = 0
-    create_ball(x, y, r, 'red', vx, vy)
+    color = 'red'
+    Ball.create(x, y, vx, vy, r, color, 'g1')
 
 
-def right_click(event):
-    # создание целей
+def mishen():
+    '''создание целей'''
     r = randint(20, 100)
+    x = randint(WIDTH / 2, WIDTH)
+    y = HEIGHT
+    vx = 0
+    vy = randint(-3, -1)
+    color = 'blue'
+    Ball.create(x, y, vx, vy, r, color, 'g2')
+    # black
+    # white
+    root.after(TIMER, mishen)
+
+
+def popal(event):
     x = event.x
     y = event.y
-    vx = randint(-2, 2)
-    vy = randint(-2, 2)
-    create_ball(x, y, r, 'blue', vx, vy)
-
-
-def create_ball(x, y, r, color, vx, vy):
-    ball_id = [battlefield.create_oval(x, y, x + r, y + r, fill=color), vx, vy]
-    objects.append(ball_id)
-    return ball_id
+    print(event.x, event.y)
+    print('Попал')
 
 
 class Ball:
-    def __init__(self, x, y, r, color, vx, vy):
-        # self.ball_id =
-        self.y = y
+    def __init__(self, x, y, vx, vy, r, color, tag):
         self.x = x
-        self.vy = vy
+        self.y = y
         self.vx = vx
+        self.vy = vy
         self.r = r
         self.color = color
+        self.tag = tag
+        self.ball_id = battlefield.create_oval(self.x, self.y, self.x + self.r, self.y + self.r, fill=self.color,
+                                               tag=self.tag)
 
     @classmethod
-    def create(cls, x, y, r, color, vx, vy):
-        return cls.__init__(x, y, r, color, vx, vy)
+    def create(cls, x, y, vx, vy, r, color, grup):
+        objects.append(cls(x, y, vx, vy, r, color, grup))
 
-
-def update(ball):
-    battlefield.move(ball[0], ball[1], ball[2])
+    def update(self):
+        battlefield.move(self.ball_id, self.vx, self.vy)
 
 
 def update_world():
     for obj in objects:
-        update(obj)
+        obj.update()
+        battlefield.tag_bind('g2', '<Button-1>', popal)
+        # "число объектов: len(objects)
     root.after(DELAY, update_world)
 
 
 # создаем главное окно и поле боя
+objects = []
 root = tk.Tk()
 root.geometry(str(WIDTH) + 'x' + str(HEIGHT))
 battlefield = tk.Canvas(root, width=WIDTH, height=HEIGHT, bg='green')
 battlefield.pack()
-battlefield.bind('<Button-1>', left_click)
-battlefield.bind('<Button-3>', right_click)
-objects = []
+battlefield.bind('<Button-3>', pulja)
+mishen()
 update_world()
 root.mainloop()
